e339a84016448ee25881c8c6a637d4eff485776c
takienravy <waldorffem@gmail.com>
2026-09-08 19:07:05 +0200

    Re-order images
    Updated alt text for images to provide more detail.

--- images.js
+++ images.js
@@ -12,9 +12,9 @@ const PHOTOS = [
   { src: "images/web-21.jpg", alt: "yellow tomatoes" },
   { src: "images/web-27.jpg", alt: "black and white interior" },
   { src: "images/web-29.jpg", alt: "cherry compote" },
-  { src: "images/web-4.jpg", alt: "black and white tree branches" },
+  { src: "images/web-4.jpg", alt: "black and white tree branches with fruit" },
   { src: "images/web-24.jpg", alt: "ivy growing" },
-  { src: "images/web-18.jpg", alt: "fallen tree over pond" },
   { src: "images/web-11.jpg", alt: "birch trees in winter in black and white" },
-  { src: "images/web-19.jpg", alt: "kitchen interior" },
+  { src: "images/web-19.jpg", alt: "kitchen interior" },  
+  { src: "images/web-18.jpg", alt: "fallen tree over pond" },
 ];
