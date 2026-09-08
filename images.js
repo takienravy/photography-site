5130541764ed655d36769f3347baefb1db91e90a
takienravy <waldorffem@gmail.com>
2026-09-08 17:52:38 +0200

    Update image entries in PHOTOS array

--- images.js
+++ images.js
@@ -4,12 +4,14 @@
 
 const PHOTOS = [
   { src: "images/web-15.jpg", alt: "black and white anemone flower" },
-  { src: "images/web-22.jpg", alt: "yolks" },
+  { src: "images/web-26.jpg", alt: "yolks" },
   { src: "images/web-6.jpg", alt: "feast" },
   { src: "images/web-8.jpg", alt: "children's sleds" },
   { src: "images/web-9.jpg", alt: "russian town in winter in black and white" },
+  { src: "images/web-28.jpg", alt: "conservatory cafe exterior" },
   { src: "images/web-21.jpg", alt: "yellow tomatoes" },
-  { src: "images/web-25.jpg", alt: "black and white interior" },
+  { src: "images/web-27.jpg", alt: "black and white interior" },
+  { src: "images/web-29.jpg", alt: "cherry compote" },
   { src: "images/web-4.jpg", alt: "black and white tree branches" },
   { src: "images/web-24.jpg", alt: "ivy growing" },
   { src: "images/web-18.jpg", alt: "fallen tree over pond" },
